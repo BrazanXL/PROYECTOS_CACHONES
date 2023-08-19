afeb1e67a670efeb731bbee557a104450b70a883
BrazanXL <joselolito2@hotmail.com>
2023-08-19 09:20:03 -0500

    pull basico

---
+++ software/src/main/java/com/mycompany/software/Software.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.software;
+
+/**
+ *
+ * @author KyB
+ */
+public class Software {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
