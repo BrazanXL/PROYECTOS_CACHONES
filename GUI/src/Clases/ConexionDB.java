e67fcdec828c9ecd709b05955f7f2b8e7d27bb5b
BrazanXL <joselolito2@hotmail.com>
2023-08-19 12:36:45 -0500

    Proyecto
    en este aparcado se trabajara todo el proyecto, el anterior solo fue una prueba.
    Ya verifique cual seria mas eficiente  y bueno por aqui se empezara con el software

---
+++ GUI/src/Clases/ConexionDB.java
@@ -0,0 +1,16 @@
+package Clases;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+/**
+ * @author Bryan Pantoja
+ * @author Ismael cajar
+ * @author hansel ortiz
+ * @author rafael camargo
+ */
+public class ConexionDB {
+    
+    
+}
