c76b1c60714f7c7e461a5b0806e2bdbaa8a973cd
Lajote <rafaelocamargo@hotmail.com>
2023-08-19 09:54:14 -0500

    habla cachon

--- software/src/main/java/com/mycompany/software/BIO_PROJECT.java
+++ software/src/main/java/com/mycompany/software/BIO_PROJECT.java
@@ -14,5 +14,6 @@ public class Mavenproject1 {
         System.out.println("Hello Worldessddds!");
         System.out.println("Recibido xd");
         System.out.println("Lajote speaking");
+        System.out.println("Habla  Cachon");
     }
 }
