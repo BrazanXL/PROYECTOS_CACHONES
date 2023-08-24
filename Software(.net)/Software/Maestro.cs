d676657be466fcc9698957afb1cb1ac87e7bd2af
BrazanXL <joselolito2@hotmail.com>
2023-08-23 22:04:55 -0500

    Se integra MVS al proyecto

---
+++ Software(.net)/Software/Maestro.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Software
+{
+    public partial class Maestro : Form
+    {
+        public Maestro()
+        {
+            InitializeComponent();
+        }
+        private void BLogin_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
