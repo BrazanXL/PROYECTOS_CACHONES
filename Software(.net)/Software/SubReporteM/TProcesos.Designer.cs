5bc8540f053b00d54166e1030620d09cba79fb6a
Lajote <rafaelocamargo@hotmail.com>
2023-08-25 18:50:02 -0500

    TProcesos ajuste
    desabilitado el controlBox completo

--- Software(.net)/Software/SubReporteM/TProcesos.Designer.cs
+++ Software(.net)/Software/SubReporteM/TProcesos.Designer.cs
@@ -40,10 +40,10 @@
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(13, 13);
-            this.checkBox1.Margin = new System.Windows.Forms.Padding(6);
+            this.checkBox1.Location = new System.Drawing.Point(17, 16);
+            this.checkBox1.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(80, 17);
+            this.checkBox1.Size = new System.Drawing.Size(95, 20);
             this.checkBox1.TabIndex = 0;
             this.checkBox1.Text = "checkBox1";
             this.checkBox1.UseVisualStyleBackColor = true;
@@ -51,10 +51,10 @@
             // checkBox2
             // 
             this.checkBox2.AutoSize = true;
-            this.checkBox2.Location = new System.Drawing.Point(13, 42);
-            this.checkBox2.Margin = new System.Windows.Forms.Padding(6);
+            this.checkBox2.Location = new System.Drawing.Point(17, 52);
+            this.checkBox2.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.checkBox2.Name = "checkBox2";
-            this.checkBox2.Size = new System.Drawing.Size(80, 17);
+            this.checkBox2.Size = new System.Drawing.Size(95, 20);
             this.checkBox2.TabIndex = 1;
             this.checkBox2.Text = "checkBox2";
             this.checkBox2.UseVisualStyleBackColor = true;
@@ -62,10 +62,10 @@
             // checkBox3
             // 
             this.checkBox3.AutoSize = true;
-            this.checkBox3.Location = new System.Drawing.Point(13, 71);
-            this.checkBox3.Margin = new System.Windows.Forms.Padding(6);
+            this.checkBox3.Location = new System.Drawing.Point(17, 87);
+            this.checkBox3.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.checkBox3.Name = "checkBox3";
-            this.checkBox3.Size = new System.Drawing.Size(80, 17);
+            this.checkBox3.Size = new System.Drawing.Size(95, 20);
             this.checkBox3.TabIndex = 2;
             this.checkBox3.Text = "checkBox3";
             this.checkBox3.UseVisualStyleBackColor = true;
@@ -73,10 +73,10 @@
             // checkBox4
             // 
             this.checkBox4.AutoSize = true;
-            this.checkBox4.Location = new System.Drawing.Point(13, 100);
-            this.checkBox4.Margin = new System.Windows.Forms.Padding(6);
+            this.checkBox4.Location = new System.Drawing.Point(17, 123);
+            this.checkBox4.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.checkBox4.Name = "checkBox4";
-            this.checkBox4.Size = new System.Drawing.Size(80, 17);
+            this.checkBox4.Size = new System.Drawing.Size(95, 20);
             this.checkBox4.TabIndex = 3;
             this.checkBox4.Text = "checkBox4";
             this.checkBox4.UseVisualStyleBackColor = true;
@@ -85,18 +85,20 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(216, 26);
+            this.label1.Location = new System.Drawing.Point(288, 32);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(91, 13);
+            this.label1.Size = new System.Drawing.Size(116, 17);
             this.label1.TabIndex = 4;
             this.label1.Text = "Observaciones";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(219, 42);
+            this.textBox1.Location = new System.Drawing.Point(292, 52);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(211, 75);
+            this.textBox1.Size = new System.Drawing.Size(280, 91);
             this.textBox1.TabIndex = 5;
             // 
             // BVolver
@@ -104,11 +106,12 @@
             this.BVolver.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.BVolver.FlatAppearance.BorderSize = 0;
             this.BVolver.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.BVolver.Font = new System.Drawing.Font("RussellSquare", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BVolver.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.BVolver.ForeColor = System.Drawing.Color.White;
-            this.BVolver.Location = new System.Drawing.Point(102, 141);
+            this.BVolver.Location = new System.Drawing.Point(136, 174);
+            this.BVolver.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.BVolver.Name = "BVolver";
-            this.BVolver.Size = new System.Drawing.Size(75, 22);
+            this.BVolver.Size = new System.Drawing.Size(100, 27);
             this.BVolver.TabIndex = 6;
             this.BVolver.Text = "Volver";
             this.BVolver.UseVisualStyleBackColor = false;
@@ -116,10 +119,11 @@
             // 
             // TProcesos
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
-            this.ClientSize = new System.Drawing.Size(456, 175);
+            this.ClientSize = new System.Drawing.Size(608, 215);
+            this.ControlBox = false;
             this.Controls.Add(this.BVolver);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
@@ -128,6 +132,9 @@
             this.Controls.Add(this.checkBox2);
             this.Controls.Add(this.checkBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "TProcesos";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "TProcesos";
