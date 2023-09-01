02cd204f70590e829fa42fbbfbbd3a42b6d1e390
Lajote <rafaelocamargo@hotmail.com>
2023-09-01 18:42:57 -0500

    ajustes

---
+++ software/src/main/java/com/mycompany/software/Software.java
@@ -0,0 +1,13 @@
+package com.mycompany.software;
+/**
+ * @author Bryan Pantoja
+ * @author Ismael cajar
+ * @author hansel ortiz
+ * @author rafael camargo
+ */
+public class Software {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
