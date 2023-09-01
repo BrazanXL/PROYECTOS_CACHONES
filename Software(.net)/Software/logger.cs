db5fa3a810f9c6d9830b9edf5effa1775c3a1de0
BrazanXL <joselolito2@hotmail.com>
2023-09-01 07:30:27 -0500

    ajustes
    Se Crea log de errores bajo el nombre de logger.cs

---
+++ Software(.net)/Software/logger.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Configuration;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Software
+{
+    public static class logger
+    {
+        public static void RegistrarError(String message) 
+        {
+            String LogPath = ConfigurationManager.AppSettings["LogPath"];
+            using (StreamWriter writer = new StreamWriter(LogPath,true)) 
+            {
+                writer.WriteLine($"{DateTime.Now}:{message}");
+                //esto se coloca para llamar esta clase ¬
+                //¬¬¬(logger.RegistrarError("Se Capturo el sigiente error: " + error);)
+            }
+        }
+    }
+}
